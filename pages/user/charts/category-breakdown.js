1a316798bc75bcf52f6063dc3d93e81545e3fd00
Marty Miguel <marty.miguel@tuitt.com>
2021-06-22 21:29:31 +0800

    created file structure

---
+++ pages/user/charts/category-breakdown.js
@@ -0,0 +1,12 @@
+import { InputGroup, Form, Col } from 'react-bootstrap'
+import { Pie } from 'react-chartjs-2'
+import View from '../../../components/View'; 
+
+
+export default () => {
+	return(
+		<View title="Category Breakdown">
+		   <h3> Category Breakdown</h3>
+		</View>
+	)
+}
