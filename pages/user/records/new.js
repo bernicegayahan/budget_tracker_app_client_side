f7eae054357fcce72b8349bab11a8d6a5591be49
Marty Miguel <marty.miguel@tuitt.com>
2021-06-24 20:42:27 +0800

    done with getting categories

--- pages/user/records/new.js
+++ pages/user/records/new.js
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { Form, Button, Row, Col, Card } from 'react-bootstrap'
 import Router from 'next/router'
 import View from '../../../components/View'
 import Swal from 'sweetalert2'
+import AppHelper from '../../../app-helper';
 
 export default () => {
     return (
@@ -22,23 +24,58 @@ export default () => {
 }
 
 const NewRecordForm = () => {
-  
+    const [ categoryName, setCategoryName ] = useState(undefined);
+    const [ typeName, setTypeName] = useState(undefined);
+    const [ amount, setAmount] = useState(0);
+    const [ description, setDescription] = useState("");
+    const [ categories, setCategories] = useState([]);
+
+    
+    const getCategories = (value) => {
+       setTypeName(value)
+
+       const payload = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${ AppHelper.getAccessToken()}`
+          },
+          body: JSON.stringify({ typeName: value })
+       }
+
+       //send a request using fetch
+       fetch(`http://localhost:4000/api/users/get-categories`, payload).then(AppHelper.toJSON).then(data => {
+            setCategories(data)
+       })
+    }
     return (
         <Form>
             <Form.Group controlId="typeName">
                 <Form.Label>Category Type:</Form.Label>
-                <Form.Control as="select" required>
+                <Form.Control as="select" value={ typeName } onChange={ (e) => getCategories(e.target.value) }required>
+
                     <option value selected disabled>Select Category</option>
                     <option value="Income">Income</option>
                     <option value="Expense">Expense</option>
                 </Form.Control>
             </Form.Group>
+
             <Form.Group controlId="categoryName">
                 <Form.Label>Category Name:</Form.Label>
-                <Form.Control as="select" required>
+                <Form.Control as="select" value={ categoryName } onChange={ (e) => setCategoryName(e.target.value)} required>
                     <option value selected disabled>Select Category Name</option>
+                    {
+                      categories.map((category) => {
+                         return (
+                            <option key={ category.id} value={ category.name}>
+                              { category.name }
+                            </option>
+                         )
+                      })
+                    }
                 </Form.Control>
             </Form.Group>
+
             <Form.Group controlId="amount">
                 <Form.Label>Amount:</Form.Label>
                 <Form.Control type="number" placeholder="Enter amount" required/>
